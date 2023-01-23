fc6e17c5e561a25360052fd2eafbe99a844d6c5a
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-01-23 16:44:59 +0700

    finish routing using class component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,12 @@
+import { Component } from "react";
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"; 
 import "./navigation.styles.scss";
 
-const Navigation = () => {
-    return (
+class Navigation extends Component {
+    render() {
+       return ( 
         <Fragment>
             <div className="navigation">
             <Link className="logo-container" to="/">
@@ -18,7 +20,8 @@ const Navigation = () => {
             </div>
             <Outlet />
         </Fragment>
-    )
-};
+        )
+    }
+}
 
 export default Navigation;
