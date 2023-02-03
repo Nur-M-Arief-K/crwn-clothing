585a76c3e3665d6f053a3d27a54c28a074153e06
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-02-03 16:25:41 +0700

    fix little bug

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,27 +15,22 @@ class Navigation extends Component {
             <CrwnLogo className="logo" />
           </Link>
           <div className="nav-links-container">
-            <Link className="nav-link" to="/shop">
-              shop
-            </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-link" to="/shop">
-                        shop
-                    </Link>
-                    {
-                        currentUser ?  (
-                            <span className="nav-link" onClick={signOutUser}>sign out</span>
-                        ) : (
-                            <Link className="nav-link" to="/auth">
-                                sign in
-                            </Link>
-                        )
-                    }
-                </div>
-            </div>
-            <Outlet />
+              <Link className="nav-link" to="/shop">
+                  shop
+              </Link>
+              {
+                  currentUser ?  (
+                      <span className="nav-link" onClick={signOutUser}>sign out</span>
+                  ) : (
+                      <Link className="nav-link" to="/auth">
+                          sign in
+                      </Link>
+                  )
+              }
           </div>
-        </Fragment>
+        </div>
+        <Outlet />
+      </Fragment>
     )
   }
 };
